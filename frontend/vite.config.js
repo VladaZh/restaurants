53ccd68458e3e6a6199826bc24f606e21f49c0b8
Vladislava Zhuikova <vladazhuikova@gmail.com>
2026-05-05 01:43:00 +0300

    fix vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/restaurants/',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        firenze: resolve(__dirname, 'firenze.html'),
+        roma: resolve(__dirname, 'roma.html'),
+      },
+    },
     outDir: 'dist',
     assetsDir: 'assets'
   }
